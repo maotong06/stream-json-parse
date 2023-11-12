fa3f3067cc1a9f69abd0880745a1a255bc1a9399
mt <maotong06@qq.com>
2023-11-12 15:53:36 +0800

    del dist

--- lib-types/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { IJSONParseConfig } from './lib/parse';
-export declare function fetchStreamJson({ url, fetchOptions, JSONParseOption }: {
-    url: string;
-    JSONParseOption: IJSONParseConfig;
-    fetchOptions?: RequestInit;
-}): Promise<void>;
--- lib-types/lib/utils.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function structuralClone(obj: any): Promise<unknown>;
-export declare function arrayEqual(a: any[], b: any[]): boolean;
-export declare function delay(ms: number): Promise<unknown>;
